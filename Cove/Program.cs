7e2df6d70a5163e6cab382806c0cdfeebf994daf
Shon Thomas <shonpt@outlook.com>
2024-11-23 12:00:22 -0900

    fix server commands to properly extract arguments

--- Cove/Program.cs
+++ Cove/Program.cs
@@ -41,7 +41,7 @@ while (true)
             break;
 
         case "say":
-            if (args.Length > 1)
+            if (inputArgs.Length >= 1)
             {
                 string message = args[1];
                 webfishingServer.MessageGlobal("Server: {Message}", message);
@@ -54,7 +54,7 @@ while (true)
             break;
 
         case "ban":
-            if (args.Length > 1)
+            if (inputArgs.Length >= 1)
             {
                 string identifier = args[1];
                 WFPlayer? player = null;
@@ -93,7 +93,7 @@ while (true)
             break;
 
         case "kick":
-            if (args.Length > 1)
+            if (inputArgs.Length > 1)
             {
                 string identifier = args[1];
                 WFPlayer? player = null;
